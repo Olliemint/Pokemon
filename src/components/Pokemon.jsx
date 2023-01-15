6c1557d4076fe0bbe44cd922ccd28e89e9fdc398
Olliemint <furywrld5@gmail.com>
2023-01-15 16:19:00 +0300

    feat: added and styled search form

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-
+import { FaSearch } from "react-icons/fa";
 const Pokemon = () => {
   return (
-      <div className='to-center'>
-          <div className="box">
-              <h1>Comming soon</h1>
+    <div className="pokemon-list">
+      <div className="main-section">
+        <div className="new-pokemon">
+          <div className="add-pokemon">
+            <p>Pokédex</p>
           </div>
-      
+        </div>
+        <div className="search-section">
+          <div className="search-form">
+            <form>
+              <label htmlFor="">Name or Number</label>
+              <br />
+              <input type="text" name="" id="" />
+              <button type="submit">
+                <FaSearch />
+              </button>
+            </form>
+          </div>
+          <div className="search-details">
+            <p>
+              Search for a Pokémon by name or using its National Pokédex number.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Pokemon
+export default Pokemon;
